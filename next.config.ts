7bbe353b9949ffc54160aa867544a3dab6d1a6a8
Alex <gemstone.merdy@gmail.com>
2026-01-29 04:25:11 +0100

    critters installation

--- next.config.ts
+++ next.config.ts
@@ -10,13 +10,13 @@ const nextConfig: NextConfig = {
     optimizePackageImports: [
       'lucide-react', // Si utilisé - regroupe les icônes
       'framer-motion', // Optimise les bundles d'animation
-      'sonner', // Votre lib de toasts
+      
     ],
     
     // 3. Scroll restoration pour UX
     scrollRestoration: true,
   },
-  
+
   images: {
     remotePatterns: [
       {
@@ -86,58 +86,7 @@ const nextConfig: NextConfig = {
   },
 
   /* Compression */
-  compress: true,
-
-  // webpack: (config, { isServer, buildId, dev, webpack }) => {
-  //   if (!isServer) {
-  //     // More comprehensive fallbacks for MongoDB dependencies
-  //     config.resolve.fallback = {
-  //       ...config.resolve.fallback,
-  //       fs: false,
-  //       net: false,
-  //       tls: false,
-  //       child_process: false,
-  //       dns: false,
-  //       'fs/promises': false,
-  //       'timers/promises': false,
-  //       'mongodb-client-encryption': false,
-  //       snappy: false,
-  //       kerberos: false,
-  //       '@mongodb-js/zstd': false,
-  //       aws4: false,
-  //       os: false,
-  //       path: false,
-  //       crypto: false,
-  //       stream: false,
-  //       util: false,
-  //       buffer: false,
-  //       url: false,
-  //       http: false,
-  //       https: false,
-  //       zlib: false,
-  //     }
-
-  //     // Ignore MongoDB packages entirely in client bundles
-  //     config.plugins.push(
-  //       new webpack.IgnorePlugin({
-  //         resourceRegExp: /^(mongodb|mongoose|bson)$/,
-  //       })
-  //     )
-
-  //     // Add rule to exclude MongoDB from being processed
-  //     config.module.rules.push({
-  //       test: /[\\/]node_modules[\\/](mongodb|mongoose|bson)[\\/]/,
-  //       use: 'null-loader',
-  //     })
-  //   }
-
-  //   return config
-  // },
-
-  // Experimental feature to help with module resolution
-  // experimental: {
-  //   serverComponentsExternalPackages: ['mongodb', 'mongoose', 'bson'],
-  // },
+  compress: true, 
 }
 
 export default nextConfig
